462b38d91ed740809b69a0b4ca0d40ef4b79aeaa
Moises Martinez <moisesheredadesbq@gmail.com>
2024-11-14 01:42:09 +0100

    Vista de bocadillo terminada

--- controllers/pedirBocadillo.js
+++ controllers/pedirBocadillo.js
@@ -11,6 +11,13 @@ document.addEventListener("DOMContentLoaded", function () {
             hacerPedido("frio");
         }
     });
+    const botonCaliente = document.getElementById("botonBocadilloCaliente");
+    botonCaliente.addEventListener("click", function () {
+        const confirmacion = confirm("¿Deseas confirmar el pedido de bocadillo caliente del día?");
+        if (confirmacion) {
+            hacerPedido("caliente");
+        }
+    });
 
     function hacerPedido(tipo) {
         const url = "sw_pedirBocadillo.php";
@@ -29,6 +36,22 @@ document.addEventListener("DOMContentLoaded", function () {
                 alert("Pedido realizado con éxito: " + json.message);
             } else {
                 alert("Error al realizar el pedido: " + json.message);
+            }
+        })
+        .catch(error => console.error("Error en la solicitud:", error));
+    }
+
+    mostrarBocadillos();
+    function mostrarBocadillos(){
+        const url = "sw_mostrarBocadillos.php"; // Mandar json de js al php
+        
+        fetch(url)
+        .then(response => response.json())
+        .then(json => {
+            console.log(json)
+            if(json.success){
+                document.getElementById('bocadilloFrio').innerHTML = `${json.data[0].nombre}`; 
+                document.getElementById('bocadilloCaliente').innerHTML = `${json.data[1].nombre}`;                             
             }
         })
         .catch(error => console.error("Error en la solicitud:", error));
--- sw_pedirBocadillo.php
+++ sw_pedirBocadillo.php
@@ -1,16 +1,17 @@
 <?php
 header('Content-Type: application/json');
 session_start();
-require 'inc/auth.inc.php'; // Verifica autenticación
-require 'models/Pedido.php'; // Incluye el modelo de Pedido
+require 'inc/auth.inc.php'; 
+require 'models/Pedido.php';
 
 $data = json_decode(file_get_contents('php://input'), true);
 $tipo_bocadillo = $data['tipo_bocadillo'] ?? null;
 
 if ($tipo_bocadillo) {
-    // Convertir el tipo de bocadillo a 1 (frío) o 0 (caliente)
+    //Convertir el tipo de bocadillo a 1 (frío) o 0 (caliente) 
+    //ya que en la BD en la columna "frio" se almacena 1 para bocadillo frío y 0 para bocadillo caliente
     $tipo_bocadillo_valor = ($tipo_bocadillo === "frio") ? 1 : 0;
-    $dia = date('l'); // Obtiene el día actual
+    $dia = date('l'); //Obtiene el día actual (En inglés)
 
     $pedido = new Pedido();
     $resultado = $pedido->registrarPedidoDelDia($_SESSION['id_usuario'], $tipo_bocadillo_valor, $dia);
