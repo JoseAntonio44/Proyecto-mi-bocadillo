ca066707c0c30d1b83c8b60ff1e495821e3ace5b
JoseAntonio44 <josantgar44@gmail.com>
2024-11-15 17:23:01 +0100

    Aplicado una ventana de tiempo en la que se puede realizar el pedido

--- sw_pedirBocadillo.php
+++ sw_pedirBocadillo.php
@@ -1,34 +1,41 @@
 <?php
 header('Content-Type: application/json');
 session_start();
-require 'inc/auth.inc.php'; 
+require 'inc/auth.inc.php';
 require 'models/Pedido.php';
 
 $data = json_decode(file_get_contents('php://input'), true);
 $tipo_bocadillo = $data['tipo_bocadillo'] ?? null;
+$hora = date('H:i');
 
-if ($tipo_bocadillo) {
-    // Convertir el tipo de bocadillo a 1 (frío) o 0 (caliente)
-    $dia = date('l'); // Obtiene el día actual
+if ($hora >= '09:00' && $hora <= '12:00') {
+    if ($tipo_bocadillo) {
+        // Convertir el tipo de bocadillo a 1 (frío) o 0 (caliente)
+        $dia = date('l'); // Obtiene el día actual
 
-    $pedido = new Pedido();
-    $resultado = $pedido->registrarPedidoDelDia($_SESSION['id_usuario'], $tipo_bocadillo, $dia);
+        $pedido = new Pedido();
+        $resultado = $pedido->registrarPedidoDelDia($_SESSION['id_usuario'], $tipo_bocadillo, $dia);
 
-    if ($resultado) {
-        echo json_encode([
-            'success' => true,
-            'message' => "Pedido de bocadillo del día realizado correctamente."
-        ]);
+        if ($resultado) {
+            echo json_encode([
+                'success' => true,
+                'message' => "Pedido de bocadillo del día realizado correctamente."
+            ]);
+        } else {
+            echo json_encode([
+                'success' => false,
+                'message' => "Error al realizar el pedido del bocadillo del dia."
+            ]);
+        }
     } else {
         echo json_encode([
             'success' => false,
-            'message' => "Error al realizar el pedido del bocadillo del dia."
+            'message' => "Tipo de bocadillo no especificado."
         ]);
     }
 } else {
     echo json_encode([
         'success' => false,
-        'message' => "Tipo de bocadillo no especificado."
+        'message' => "A esta hora no puedes pedir un bocadillo."
     ]);
 }
-?>
